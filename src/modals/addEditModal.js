09936b9fc9fcf232a0ab12e06ccbb43217271b81
Marcin Pec <Marcin_Pec@epam.com>
2020-08-28 12:53:08 +0200

    modal refactoring

--- src/modals/addEditModal.js
+++ src/modals/addEditModal.js
@@ -1,22 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './addEditModal.css'
 
-class AddEditModal extends React.Component {
-
-    render(){
-        return (
-            <div className={'addEditContainer' + (this.props.isVisible ? ' visible' : '')}>
-                <div className="addEditModal">
-                    EDIT!!!!
-                    <button className="close" onClick={() => this.props.toggleModal()}>X</button>
-                    <div className="header">delete movie</div>
-                    <div className="body">Are you sure you want to delete this movie?</div>
-                    <button className="confirm" onClick={() => this.props.toggleModal()}>confirm</button>
-                </div>
+const AddEditModal = (props) => {
+    return (
+        <div className={'addEditContainer' + (props.isVisible ? ' visible' : '')}>
+            <div className="addEditModal">
+                EDIT!!!!
+                    <button className="close" onClick={() => props.toggleModal()}>X</button>
+                <div className="header">delete movie</div>
+                <div className="body">Are you sure you want to delete this movie?</div>
+                <button className="confirm" onClick={() => props.toggleModal()}>confirm</button>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+AddEditModal.propTypes = {
+    toggleModal: PropTypes.func.isRequired,
+    isVisible: PropTypes.bool.isRequired
+};
 
 export default AddEditModal;
--- src/modals/deleteConfirmation.js
+++ src/modals/deleteConfirmation.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './deleteConfirmation.css'
 
-class DeleteConfirmation extends React.Component {
-
-    render(){
-        return (
-            <div className={'deleteConfirmation' + (this.props.isVisible ? ' visible' : '')}>
-                <div className="confirmModal">
-                    <button className="close" onClick={() => this.props.toggleConfirmDeleteModal(null)}>X</button>
-                    <div className="header">delete movie</div>
-                    <div className="body">Are you sure you want to delete this movie?</div>
-                    <button className="confirm" onClick={() => this.props.toggleConfirmDeleteModal(null)}>confirm</button>
-                </div>
+const DeleteConfirmation = (props) => {
+    return (
+        <div className={'deleteConfirmation' + (props.isVisible ? ' visible' : '')}>
+            <div className="confirmModal">
+                <button className="close" onClick={() => props.toggleConfirmDeleteModal(null)}>X</button>
+                <div className="header">delete movie</div>
+                <div className="body">Are you sure you want to delete this movie?</div>
+                <button className="confirm" onClick={() => props.toggleConfirmDeleteModal(null)}>confirm</button>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+DeleteConfirmation.propTypes = {
+    toggleConfirmDeleteModal: PropTypes.func.isRequired,
+    isVisible: PropTypes.bool.isRequired
+};
 
 export default DeleteConfirmation;
