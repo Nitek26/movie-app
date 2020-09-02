8d467bbb9137f55c73aa27352bc732f5976365f2
Marcin Pec <Marcin_Pec@epam.com>
2020-09-02 10:53:56 +0200

    styling

--- src/content/searchSorter.js
+++ src/content/searchSorter.js
@@ -4,17 +4,17 @@ import './searchSorter.css';
 
 const SearchSorter = (props) => {
     const options = [
-        {value: 'release_date', title: 'Release Date'},
-        {value: 'title', title: 'Title'},
+        { value: 'release_date', title: 'Release Date' },
+        { value: 'title', title: 'Title' },
     ];
 
     return (
         <div className="searchSorter">
             <div>Sort by</div>
             <select onChange={(event) => props.setSortBy(event.target.value)} value={props.sortBy}>
-            {options.map(option => {
-                return <option key={option.value} value={option.value}>{option.title}</option>
-            })}
+                {options.map(option => {
+                    return <option key={option.value} value={option.value}>{option.title}</option>
+                })}
             </select>
         </div>
     );
